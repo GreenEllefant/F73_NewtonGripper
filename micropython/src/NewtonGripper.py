b7f4472b264121841bc65d1cebbbb306c76f4caf
GreenEllefant <germanjellsworth@gmail.com>
2023-03-24 16:33:25 -0700

    Successfully tested NewtonGripper

--- micropython/src/NewtonGripper.py
+++ micropython/src/NewtonGripper.py
@@ -5,7 +5,8 @@
     @date 2023.03.23
 '''
 
-import pyb 
+import pyb
+
 class NewtonGripper:
     '''! This class controls a Blue Robotics Newton Gripper.'''
     def __init__ (self, pwmPin, timer):
@@ -18,30 +19,30 @@ class NewtonGripper:
         self.timer = timer
         
         # Save these values for use in functions
-        self.status = 0
+        self.status = 0         # Reports which mode the actuator is in
         self.prescaler = 9      # Value for the timer's prescaler
         self.period = 12000     # Value for the timer's AR (auto reload)
                 
-        # Initialize timer to use channel 1
+        # Initialize timer to use channel 1 with PWM centered
         self.timer.deinit()
-        self.timer.init(prescaler=self.prescaler, period=self.period, mode=Timer.CENTER)
-        self.ch1 = self.timer.channel(1, mode=Timer.PWM, pin=self.pin)
+        self.timer.init(prescaler=self.prescaler, period=self.period, mode=pyb.Timer.CENTER)
+        self.ch1 = self.timer.channel(1, mode=pyb.Timer.PWM, pin=self.pin)
         
         
     def extend(self):
         '''! Runs the pwm signal to cause the actuator to extend.'''
         self.status = 1
-        self.ch1.pulse_width = 6200
+        self.ch1.pulse_width(5000)   # 5000 ticks = 1.2507ms on oscilloscope
         
     def retract(self):
         '''! Runs the pwm signal to cause the actuator to retract.'''
         self.status = -1
-        self.ch1.pulse_width = 5800
+        self.ch1.pulse_width(7000)   # 7000 ticks = 1.7510ms on oscilloscope
         
     def stop(self):
         '''! Runs the pwm signal to cause the actuator to stop moving.'''
         self.status = 0
-        self.ch1.pulse_width = 6000
+        self.ch1.pulse_width(6000)   # 6000 ticks = 1.5008ms on oscilloscope
         
     def get_status(self):
         '''! Gives the status of the NewtonGripper. 1 for extending, 0 for stopped, -1 for retracting.'''
@@ -55,17 +56,20 @@ if __name__ == "__main__":
         Connect yellow logic line to pin PB4.
     '''
     # Create but do not initialize a timer object for timer channel 3, which uses pin PB4 for channel 1 output.
-    timer3 = pyb.timer(3)
+    timer4 = pyb.Timer(4)
     
-    # Set up the pin PB4 for timer 3 channel 1 output
-    pin_PB4 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.ALT, pyb.Pin.PULL_NONE, alt=2)
-        # identifier = pyb.Pin.board.PB4
+    # Set up the pin PB6 for timer 4 channel 1 output
+    pin_PB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.OUT_PP)
+        # identifier = pyb.Pin.board.PB6
         # mode = pyb.Pin.ALT to use alternate functions (see STM32L476xx Alternate Function table)
         # pull = pyb.Pin.PULL_NONE to disable pullup resistors. Not needed for this application, just use push pull
         # alt = 2 sets alternate function to 2 (see STM32L476xx Alternate Function table)
         
     # Create NewtonGripper object
-    newt = NewtonGripper(pin_PB4, timer3)
+    newt = NewtonGripper(pin_PB6, timer4)
     
     # Run tests
+    newt.retract()
+    print( "newt.retract()" )
+    
     
